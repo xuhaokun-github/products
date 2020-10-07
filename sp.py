52a5cd08a07d7bded7e5c16314424c8746b62696
xuhaokun <kingxuhaokun@gmail>
2020-10-08 00:06:18 +0800

    csv upgrade

--- sp.py
+++ sp.py
@@ -4,8 +4,13 @@ while True:
 	if name == 'q':
 		break
 	price = input('请输入商品价格：')
+	price = int(price)
 	products.append([name,price])
 print(products)
 for p in products:
 	print(p[0], '的价格是：', p[1])
 
+with open('products.csv', 'w') as f:
+	f.write('商品,价格\n')
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n')
